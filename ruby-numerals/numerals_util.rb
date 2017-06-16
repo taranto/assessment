f6a9e588749d9c0b64c98be2cf3b39c61aabe1fc
Fernando Taranto <fertaranto@gmail.com>
2017-06-15 22:19:09 -0300

    quickfix: variables abbreviation

--- ruby-numerals/numerals_util.rb
+++ ruby-numerals/numerals_util.rb
@@ -1,52 +1,52 @@
 
 class NumeralsUtil
-	def self.numeral_right_part num, cut_position
-		num.to_s[cut_position..-1].to_i
+	def self.numeral_right_part num, cut_pos
+		num.to_s[cut_pos..-1].to_i
 	end
 
-	def self.numeral_left_part num, cut_position
-		num.to_s[0..cut_position-1].to_i
+	def self.numeral_left_part num, cut_pos
+		num.to_s[0..cut_pos-1].to_i
 	end
 
-	def self.numeral_fraction num, cut_position_begin, cut_position_end
-		num.to_s[cut_position_begin..cut_position_end-1].to_i
+	def self.numeral_fraction num, cut_pos_begin, cut_pos_end
+		num.to_s[cut_pos_begin..cut_pos_end-1].to_i
 	end
 
-	def self.add_end_space_if_required partial_translation, space_required
-		add_end_something_if_required(partial_translation, space_required, ' ')
+	def self.add_end_space_if_required partial_transl, space_req
+		add_end_something_if_required(partial_transl, space_req, ' ')
 	end
 
-	def self.add_end_minus_if_required partial_translation, space_required
-		add_end_something_if_required(partial_translation, space_required, '-')
+	def self.add_end_minus_if_required partial_transl, space_req
+		add_end_something_if_required(partial_transl, space_req, '-')
 	end
 
-	def self.add_end_something_if_required partial_translation, space_required, something
-		if (space_required) 
-			partial_translation = partial_translation + something
+	def self.add_end_something_if_required partial_transl, space_req, something
+		if (space_req) 
+			partial_transl = partial_transl + something
 		end
-		partial_translation
+		partial_transl
 	end
 
-	def self.add_prefix_if_not_null partial_translation, prefix
-		if (partial_translation.to_s.length > 0) 
-			partial_translation = partial_translation + prefix
+	def self.add_prefix_if_not_null partial_transl, prefix
+		if (partial_transl.to_s.length > 0) 
+			partial_transl = partial_transl + prefix
 		end
-		partial_translation
+		partial_transl
 	end
 
-	def self.add_begin_space_if_not_null partial_translation
-		add_begin_something_if_not_null(partial_translation, ' ')
+	def self.add_begin_space_if_not_null partial_transl
+		add_begin_something_if_not_null(partial_transl, ' ')
 	end
 
-	def self.add_begin_AND_if_not_null partial_translation
-		add_begin_something_if_not_null(partial_translation, 'and ')
+	def self.add_begin_AND_if_not_null partial_transl
+		add_begin_something_if_not_null(partial_transl, 'and ')
 	end
 
-	def self.add_begin_something_if_not_null partial_translation, something
-		if (partial_translation.to_s.length > 0) 
-			partial_translation = something + partial_translation
+	def self.add_begin_something_if_not_null partial_transl, something
+		if (partial_transl.to_s.length > 0) 
+			partial_transl = something + partial_transl
 		end
-		partial_translation
+		partial_transl
 	end
 
 	def self.add_AND_before_last_not_null_part part1, part2, part3
